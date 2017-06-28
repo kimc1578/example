b49bd246d3bcca7f53322955f2ffd1fe10df0344
bit-user <bit-user@DESKTOP-MJQQM30>
2017-06-28 18:35:53 +0900

    Test

--- DemoTestWeb/src/main/java/kr/co/bit/controller/user/UserController.java
+++ DemoTestWeb/src/main/java/kr/co/bit/controller/user/UserController.java
@@ -2,7 +2,7 @@ package kr.co.bit.controller.user;
 
 import org.springframework.stereotype.Controller;
 
-@Controller
+
 public class UserController {
 
 }
--- DemoTestWeb/src/main/java/kr/co/bit/domain/dao/user/UserDao.java
+++ DemoTestWeb/src/main/java/kr/co/bit/domain/dao/user/UserDao.java
@@ -2,7 +2,7 @@ package kr.co.bit.domain.dao.user;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface UserDao {
 
 }
